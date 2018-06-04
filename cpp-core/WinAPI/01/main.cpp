d7e34afe2346247bf190eda4e423e38ee321986d
hard <harleydav@mail.ru>
2018-06-04 17:21:20 +0300

    win api

--- cpp-core/WinAPI/01/main.cpp
+++ cpp-core/WinAPI/01/main.cpp
@@ -1,7 +1,7 @@
 #include <windows.h>
 
 // https://rsdn.org/article/baseserv/api32.xml
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCommandLine, int nCommandShow) {
+int APIENTRY WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCommandLine, int nCommandShow) {
     MessageBox(NULL, "Hello World", "WinAPI App", NULL);
 
     return 0;
